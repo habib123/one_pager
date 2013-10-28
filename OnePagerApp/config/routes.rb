15d31cae2371cbbfd06667480486bb8d8b3cdb47
stefanostam <stefanostam@gmail.com>
2013-10-28 07:00:53 -0700

    Added boostrap,added tabs for log in and try out, added the user controller

--- OnePagerApp/config/routes.rb
+++ OnePagerApp/config/routes.rb
@@ -1,4 +1,5 @@
 OnePagerApp::Application.routes.draw do
+  get "users/start"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
