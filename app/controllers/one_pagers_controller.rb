b278992c7ec6d8aa56bffaeedcc40acd384081df
stefanostam <stefanostam@gmail.com>
2013-11-28 15:03:24 -0800

    Added view for portfolio and patent_bulk onepager

--- app/controllers/one_pagers_controller.rb
+++ app/controllers/one_pagers_controller.rb
@@ -19,32 +19,59 @@ class OnePagersController < ApplicationController
 	def create
 		puts "Create started"
 		if type_param[:type] == "patent"
-
+			puts "one_pager is patent"
+			@one_pager = OnePager.new(one_pager_patent_params)
+			if @one_pager.save
+				puts "one_pager saved"
+				# TODO : Success
+			else
+				puts "Cannot save one pager"
+				# TODO : Fail
+				@type = "patent"
+				render 'new', {:type => @type}
+			end
 		elsif type_param[:type] == "portfolio"
-
+			puts "one_pager is portfolio"
+			@one_pager = OnePager.new(one_pager_portfolio_params)
+			if @one_pager.save
+				puts "one_pager saved"
+				# TODO : Success
+			else
+				puts "Cannot save one pager"
+				# TODO : Fail
+				@type = "portfolio"
+				render 'new', {:type => @type}
+			end
 		elsif type_param[:type] == "patent_bulk"
-			
+			puts "one_pager is patent_bulk"
+			@one_pager = OnePager.new(one_pager_patent_bulk_params)
+			if @one_pager.save
+				puts "one_pager saved"
+				# TODO : Success
+			else
+				puts "Cannot save one pager"
+				# TODO : Fail
+				@type = "patent_bulk"
+				render 'new', {:type => @type}
+			end			
 		elsif type_param[:type] == "company_tags"
 			
-		end
-		#@one_pager = OnePager.new(one_pager_params)
-		puts "bef"
-		puts @one_pager.email
-		puts "aft"
-		if @one_pager.save
-			# TODO : Success
-		else
-			# TODO : Fail
-			@type = "patent"
-			render 'new', {:type => @type}
 		end
 	end
 
 	private
 
-		def one_pager_params
+		def one_pager_patent_params
 			params.require(:one_pager).permit(:patent_name, :single_patent_num, :email)
 		end
+
+		def one_pager_portfolio_params
+			params.require(:one_pager).permit(:patent_name, :patent_list, :email)
+		end
+
+		def one_pager_patent_bulk_params
+			params.require(:one_pager).permit(:patent_name, :patent_list, :email)
+		end		
 
 		def type_param
 			params.require(:one_pager).permit(:type)
--- app/models/one_pager.rb
+++ app/models/one_pager.rb
@@ -1,6 +1,5 @@
 class OnePager < ActiveRecord::Base
 	validates :patent_name, presence: true
-	validates :single_patent_num, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
