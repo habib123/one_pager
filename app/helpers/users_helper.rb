c63288e0fca32784e7432603270cadf72b5c6209
Moinul Al-Mamun <moinulkuet@gmail.com>
2013-12-02 15:02:14 +0100

    remove signed_in method from users_helper
    It's implemented in SessionHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
 
 	# This function must return whether a user is signed in or not.
-	def signed_in?
+  #this function was implementaed in SessionHelper, so it's commented here
+	#def signed_in?
 		#Implementation pending
-	end
+	#end
 
 end
