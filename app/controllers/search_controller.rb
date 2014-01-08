3d96ee689a29885a0bdb3fa25cb77fd9f598dd83
zeesh_000 <zeeshan@ubuntu.(none)>
2014-01-08 01:51:16 +0100

    Search functionality enhanced.........

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,29 +9,32 @@ class SearchController < ApplicationController
     search_text_array = params['search-text'].split("\r\n")
     result_array = []
     
-    if !search_text_array.empty?
-    
-      search_text_array.each do |search_text|
+      if !search_text_array.empty?
+        then
+        search_text_array.each do |search_text|
+        type_search_result_by_mig_status = OnePager.where(type: search_criteria).where("mig_status like ?", "%#{search_text}%")
+        type_search_result_by_pat_num = OnePager.where(type: search_criteria).where("single_patent_num like ?", "%#{search_text}%")
         type_search_result_by_name = OnePager.where(type: search_criteria).where("patent_name like ?", "%#{search_text}%")
-      
-        by_tags = OnePagersTag.where("tag like ?", "%#{search_text}%")
         
+        by_tags = OnePagersTag.where("tag like ?", "%#{search_text}%")       
         #by_tags = OnePagersTag.where(tag: search_text)
         uniq_tag = by_tags.uniq {|p| p.one_pager_id}
         uniq_tag_patent_ids = uniq_tag.map(&:one_pager_id)
         result_by_tag_main = OnePager.find(uniq_tag_patent_ids)
         
         type_search_result_by_name = type_search_result_by_name + result_by_tag_main
-        result_array = result_array + type_search_result_by_name 
+        result_array = result_array + type_search_result_by_name
+        result_array = result_array + type_search_result_by_pat_num
+        result_array = result_array + type_search_result_by_mig_status
         @result = result_array.uniq {|p| p.id}
-        @result = Kaminari.paginate_array(@result).page(params[:page]).per(10) 
+        @result = Kaminari.paginate_array(@result).page(params[:page]).per(10)
       end
-    else
+                  
+      else
       type_search_result_by_name = OnePager.where(type: search_criteria)
-      result_array = type_search_result_by_name
-      @result = result_array.uniq {|p| p.id}
-      @result = @result.page(params[:page]).per(10)
-    end  
-    
-  end
+        result_array = type_search_result_by_name
+        @result = result_array.uniq {|p| p.id}
+        @result = @result.page(params[:page]).per(10)
+      end
+   end
 end
