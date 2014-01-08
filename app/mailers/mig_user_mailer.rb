c30b0457414cba477e8d66983efc5783de006df6
habib_tum <habib_tum@ymail.com>
2014-01-08 20:18:12 +0100

    Revert "my first commit"
    This reverts commit 1b9142d7322e6cf1e1518eb209cdd412b4652572.

--- app/mailers/mig_user_mailer.rb
+++ app/mailers/mig_user_mailer.rb
@@ -1,13 +1,9 @@
 class MigUserMailer < ActionMailer::Base
-  default from: "munichinnovationgmbh@gmail.com"
+   default from: "munichinnovationgmbh@gmail.com"
   ONEPAGER_HOST = 'http://one-pager-mig.herokuapp.com'
 
-  def mig_notification_email_approve(email)
-  	#@url = 'http://' + host + Rails.application.routes.url_helpers.one_pagers_path + '/' + id
-  	mail(to:email, subject: 'Your request has been approved')
-  end
-  def mig_notification_email_deny(email)
-  	#@url = 'http://' + host + Rails.application.routes.url_helpers.one_pagers_path + '/' + id
-  	mail(to:email, subject: 'Your request has been denied')
+  def new_one_pager(mail,host,id)
+  	@url = 'http://' + host + Rails.application.routes.url_helpers.one_pagers_path + '/' + id
+  	mail(to:mail, subject: 'New One Pager Request')
   end
 end
